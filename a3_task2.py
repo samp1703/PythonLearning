c6ed377d0e53233dc89de5eed06e249272e29592
samp1703 <sampathps001@gmail.com>
2025-11-06 10:20:59 +0530

    task2 updated

---
+++ a3_task2.py
@@ -0,0 +1,12 @@
+import math
+
+num = int(input("Enter a number: "))
+
+sq_root = math.sqrt(num)
+print(f"Square root : {sq_root}")
+
+natural_log = math.log(num)
+print(f"Logarithm : {natural_log}")
+
+sine_value = math.sin(num)
+print(f"Sine : {sine_value}")
